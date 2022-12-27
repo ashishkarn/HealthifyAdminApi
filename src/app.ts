c5f40e6695f1c3d93bb91b7c0536b998eb3dd8b5
ashish <ashish@pop-os.localdomain>
2022-12-28 03:04:03 +0530

    wtf

--- src/app.ts
+++ src/app.ts
@@ -17,15 +17,6 @@ app.use(
     origin: "*"
   })
 )
-app.use(async (ctx, next) => {
-  ctx.set("Access-Control-Allow-Origin", "*")
-  ctx.set(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  )
-  ctx.set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
-  await next()
-})
 app.use(globalErrorHandler)
 app.use(bodyParser())
 app.use(router.routes())
